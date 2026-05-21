323b93143eeeda05440eec44bdad28e51a383440
Kaushik Kumar Sahoo <sahookaushik1797@gmail.com>
2026-05-21 16:03:37 +0530

    Add 06-email-queue-redis-list source code

---
+++ 06-email-queue-redis-list/src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import Redis from 'ioredis';
+
+const app = express();
+app.use(express.json());
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379')
+
+const QUEUE_KEY = 'queue:emails';
+
+app.post('/emails', async (req, res) => {
+    const job = {
+        to: req.body.to,
+        subject: req.body.subject || 'NO SUBJECT',
+        body: req.body.body || 'NO CONTENT',
+        createdAt: new Date().toISOString()
+    }
+    await redis.lpush(QUEUE_KEY, JSON.stringify(job));
+    res.json({ queued: true, job });
+})
+
+app.get('/emails/process-one', async (req, res) => {
+    const rawJob = await redis.rpop(QUEUE_KEY)
+    if (!rawJob) {
+        return res.json({ message: "no jobs in the queue" })
+    }
+    const job = JSON.parse(rawJob);
+    res.json({ message: 'EMAIL SENT', job })
+})
+
+app.listen(3000, () => {
+    console.log("SERVER IS RUNNING !!!")
+})
